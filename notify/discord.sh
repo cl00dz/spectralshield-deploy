c452178fef7d61fbc2042405d161b73dee6e85b8
cl00dz <152516715+cl00dz@users.noreply.github.com>
2025-10-30 14:36:03 -0400

    Add Discord webhook notification script

--- notify/discord.sh
+++ notify/discord.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+WEBHOOK="$1"
+shift
+MSG="$*"
+curl -s -H "Content-Type: application/json" -X POST -d "{\"content\": \"${MSG//\"/\\\"}\"}" "$WEBHOOK" >/dev/null
 
