e2b076b2ddec448752a6f9a864667f8ddb60fee1
cl00dz <152516715+cl00dz@users.noreply.github.com>
2025-10-30 14:51:08 -0400

    Delete notify directory

--- notify/discord.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-WEBHOOK="$1"
-shift
-MSG="$*"
-curl -s -H "Content-Type: application/json" -X POST -d "{\"content\": \"${MSG//\"/\\\"}\"}" "$WEBHOOK" >/dev/null
-
