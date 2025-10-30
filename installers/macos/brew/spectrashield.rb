32c0246588bca4e851b8074a7d3d6e6a06759674
cl00dz <152516715+cl00dz@users.noreply.github.com>
2025-10-30 17:25:33 -0400

    Update spectrashield.rb

--- installers/macos/brew/spectrashield.rb
+++ installers/macos/brew/spectrashield.rb
@@ -1,1 +1,17 @@
+class Spectrashield < Formula
+  desc "Self-hosted audio watermarking app"
+  homepage "https://github.com/cl00dz/spectralshield-deploy"
+  url "https://github.com/cl00dz/spectralshield-deploy/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "<FILL_TARBALL_SHA256>"
+  license "MIT"
 
+  depends_on "docker"
+
+  def install
+    bin.install "deploy.sh" => "spectrashield"
+  end
+
+  test do
+    system "#{bin}/spectrashield", "--help"
+  end
+end
